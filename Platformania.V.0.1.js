73e4821e2948ea7eccbc9c0f680aacf092d069d6
S1lkyWebb3r <hudson_asher@icloud.com>
2025-12-15 13:46:43 +0100

    LV14

--- Platformania.V.0.1.js
+++ Platformania.V.0.1.js
@@ -381,11 +381,11 @@ let objects = [
   {x: 315, y: 225, sizeWidth: 20, sizeHeight: 20, type:  "exit", color: "mediumblue", level: 14},
   //moving enemy
   {spawnX: 0, spawnY: 550, x: 0, y: 550, speedX: 3, speedY: 0, interval: 200, inInterval: 200, sizeWidth: 50, sizeHeight: 10, type: "mEnemy", color: "red", level: 14, dir: 1, inDir: 1, tick: 0},
-  {spawnX: 0, spawnY: 425, x: 0, y: 425, speedX: 3, speedY: 0, interval: 200, inInterval: 200, sizeWidth: 50, sizeHeight: 10, type: "mEnemy", color: "red", level: 14, dir: 1, inDir: 1, tick: 0},
+  {spawnX: 550, spawnY: 425, x: 0, y: 425, speedX: 3, speedY: 0, interval: 200, inInterval: 200, sizeWidth: 50, sizeHeight: 10, type: "mEnemy", color: "red", level: 14, dir: -1, inDir: -1, tick: 0},
   {spawnX: 0, spawnY: 300, x: 0, y: 300, speedX: 3, speedY: 0, interval: 200, inInterval: 200, sizeWidth: 50, sizeHeight: 10, type: "mEnemy", color: "red", level: 14, dir: 1, inDir: 1, tick: 0},
-  {spawnX: 0, spawnY: 175, x: 0, y: 175, speedX: 3, speedY: 0, interval: 200, inInterval: 200, sizeWidth: 50, sizeHeight: 10, type: "mEnemy", color: "red", level: 14, dir: 1, inDir: 1, tick: 0},
-  {x: 410, y: 570, sizeWidth: 20, sizeHeight: 20, type:  "invTeleporter", color: "mediumspringgreen", level: 14, landX: 580, landY: 580},
-  {x: 580, y: 580, sizeWidth: 20, sizeHeight: 20, type:  "exit", color: "mediumspringgreen", level: 14},
+  {spawnX: 550, spawnY: 175, x: 0, y: 175, speedX: 3, speedY: 0, interval: 200, inInterval: 200, sizeWidth: 50, sizeHeight: 10, type: "mEnemy", color: "red", level: 14, dir: -1, inDir: -1, tick: 0},
+  {x: 410, y: 570, sizeWidth: 20, sizeHeight: 20, type:  "invTeleporter", color: "mediumspringgreen", level: 14, landX: 580, landY: 570},
+  {x: 580, y: 570, sizeWidth: 20, sizeHeight: 20, type:  "exit", color: "mediumspringgreen", level: 14},
 
 ];
 
