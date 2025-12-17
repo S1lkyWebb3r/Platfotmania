ef1b4b580e57324370ced5cf665317060914ad21
S1lkyWebb3r <hudson_asher@icloud.com>
2025-12-17 11:49:42 +0100

    LV17

--- Platformania.V.0.1.js
+++ Platformania.V.0.1.js
@@ -446,7 +446,9 @@ let objects = [
   {x: 280, y: 220, sizeWidth: 25, sizeHeight: 10, type: "jumper", color: "brown", level: 17, accelX: 0},
   {x: 180, y: 220, sizeWidth: 25, sizeHeight: 10, type: "jumper", color: "brown", level: 17, accelX: 0},
   {x: 80, y: 220, sizeWidth: 25, sizeHeight: 10, type: "jumper", color: "brown", level: 17, accelX: 0},
-  {x: 40, y: 120, sizeWidth: 550, sizeHeight: 10, type: "jumper", color: "brown", level: 17, accelX: -60},
+  {x: 40, y: 120, sizeWidth: 550, sizeHeight: 10, type: "jumper", color: "brown", level: 17, accelX: -50},
+  {x: 100, y: 520, sizeWidth: 350, sizeHeight: 5, type: "enemy", color: "red", level: 17},
+
 ];
 
 // Keys pressed 
