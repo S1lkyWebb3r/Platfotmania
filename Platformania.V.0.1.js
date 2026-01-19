5d35156fc64b5b07a8e1cb4e98cd8bfbb239b66b
Sidon39 <hudson_asher@icloud.com>
2026-01-19 11:47:35 +0100

    LV1925

--- Platformania.V.0.1.js
+++ Platformania.V.0.1.js
@@ -533,6 +533,9 @@ let objects = [
   {x: 295, y: 50, sizeWidth: 10, sizeHeight: 200, type: "enemy", color: "red", level: 19.25},
   {x: 0, y: 0, sizeWidth: 560, sizeHeight: 10, type: "enemy", color: "red", level: 19.25},
   {x: 75, y: 375, sizeWidth: 150, sizeHeight: 150, type: "enemy", color: "red", level: 19.25},
+  {x: 75, y: 75, sizeWidth: 150, sizeHeight: 150, type: "enemy", color: "red", level: 19.25},
+  {x: 375, y: 375, sizeWidth: 150, sizeHeight: 150, type: "enemy", color: "red", level: 19.25},
+  {x: 375, y: 75, sizeWidth: 150, sizeHeight: 150, type: "enemy", color: "red", level: 19.25},
 ];
 
 // Keys pressed 
