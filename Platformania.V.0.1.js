aa67097516a26c70bd2446d11753c17a85ac6d42
Sidon39 <hudson_asher@icloud.com>
2026-01-19 15:49:39 +0100

    LV!)25

--- Platformania.V.0.1.js
+++ Platformania.V.0.1.js
@@ -365,7 +365,7 @@ const level19Platforms = [
 const level1925Platforms = [
  { x: 0, y: 550, sizeWidth: 150, sizeHeight: 50, name: "p1" },
  { x: 0, y: 430, sizeWidth: 10, sizeHeight: 10, name: "p1" },
- { x: 130, y: 375, sizeWidth: 10, sizeHeight: 10, name: "p2" },
+ { x: 150, y: 375, sizeWidth: 10, sizeHeight: 10, name: "p2" },
  { x: 285, y: 450, sizeWidth: 10, sizeHeight: 10, name: "p3" },
 ];
 
