cf128928235a64f50b3076f0b377cb6c69a4345e
Sidon39 <hudson_asher@icloud.com>
2026-02-23 14:14:14 +0100

    LV19.5

--- Platformania.V.0.1.js
+++ Platformania.V.0.1.js
@@ -567,7 +567,10 @@ let objects = [
   //Level 19.5 
   {x: 0, y: 530, sizeWidth: 20, sizeHeight: 20, type:  "teleporter", color: "orange", level: 19.5, landX: 0, landY: 10},
   {x: 0, y: 10, sizeWidth: 20, sizeHeight: 20, type:  "exit", color: "orange", level: 19.5},
+  {x: 80, y: 580, sizeWidth: 20, sizeHeight: 20, type:  "teleporter", color: "mediumblue", level: 19.5, landX: 0, landY: 10},
+  {x: 150, y: 10, sizeWidth: 20, sizeHeight: 20, type:  "exit", color: "mediumblue", level: 19.5},
   {x: 295, y: 0, sizeWidth: 10, sizeHeight: 600, type: "enemy", color: "red", level: 19.5},
+  {x: 130, y: 0, sizeWidth: 10, sizeHeight: 600, type: "enemy", color: "red", level: 19.5},
   {x: 0, y: 480, sizeWidth: 80, sizeHeight: 10, type: "enemy", color: "red", level: 19.5},
   {x: 70, y: 480, sizeWidth: 10, sizeHeight: 120, type: "enemy", color: "red", level: 19.5},
 ];
